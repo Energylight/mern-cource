f7e08d69ab09007bba903afce6e3a937d0d0b7e7
Roman Akh <energylight87@gmail.com>
2023-03-17 23:17:10 +0500

    prod

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Подключение необходимых модулей
 const express = require('express')
 const config = require('config')
+const path = require('path')
 const mongoose = require('mongoose')
 
 // Создание приложения
@@ -13,6 +14,13 @@ app.use(express.json({ extended: true }))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/redirect.routes'))
+
+if (process.env.NODE_ENV === 'poduction'){
+    app.use('/', express.static(path.join(__dirname, 'client', 'build'))) 
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    } )
+}
 
 // Установка порта на котором будет запущено приложение, берется из конфига, если не указан, то по умолчанию 3000
 const PORT = config.get('port') || 3000
